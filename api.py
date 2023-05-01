cdae8ead647dd776a5e49fad3301ee7b6ac52286
aayush18602 <83156674+aayush18602@users.noreply.github.com>
2023-05-01 19:11:26 +0530

    Some changes

--- api.py
+++ api.py
@@ -1,11 +1,15 @@
 from featureExtractor import PredictURL
-classification = PredictURL()
-
 
 from fastapi import FastAPI
+
+import validators
+
+classification = PredictURL()
 app = FastAPI()
 
 @app.get("/api")
-def hello(url: str):
+def hello(url: str=""):
+  if not validators.url(url):
+    return {'msg':'Invalid URL'}
   ans = classification.predict(url)
   return {ans}
