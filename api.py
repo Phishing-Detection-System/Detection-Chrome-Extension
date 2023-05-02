d1d9239a6b600b6196dd1a412de1e551e3a64af4
aayush18602 <83156674+aayush18602@users.noreply.github.com>
2023-05-02 21:09:55 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,21 @@
 from featureExtractor import PredictURL
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import validators
 
 classification = PredictURL()
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api")
 def hello(url: str=""):
